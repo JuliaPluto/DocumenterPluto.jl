8456b6866da7a62693c3c6252d0e6bb5b74d56ba
Fons van der Plas <fonsvdplas@gmail.com>
2026-04-20 20:39:47 +0200

    Update domify.jl

--- src/domify.jl
+++ src/domify.jl
@@ -3,26 +3,21 @@
 # followed by the <pluto-editor> placeholder that the JS in <head> hydrates.
 
 function Documenter.HTMLWriter.domify(dctx::DCtx, ::Node, block::PlutoBlock)
-    DOM.@tags script div progress pluto_editor
-
     page_url = Documenter.HTMLWriter.get_url(dctx.ctx, dctx.navnode.page)
     nb_href    = _resolve_href(page_url, block.resolved_notebook)
     state_href = _resolve_href(page_url, block.resolved_state)
 
     launch_params = """
     window.pluto_notebookfile = $(_js_string(nb_href));
-    window.pluto_disable_ui = $(block.disable_ui ? "true" : "false");
-    window.pluto_binder_url = $(block.binder === nothing ? "undefined" : _js_string(block.binder));
+    window.pluto_disable_ui = $(_js_string(block.disable_ui));
+    window.pluto_binder_url = $(_js_string(block.binder));
     window.pluto_statefile = $(_js_string(state_href));
     window.pluto_recording_url = undefined;
     window.pluto_recording_audio_url = undefined;
     """
 
-    # DOM.@tags doesn't allow `pluto-editor` as an identifier, so we use
-    # `pluto_editor` and fix the tag name with a Tag() override. The frontend
-    # matches on the hyphenated custom element name.
     editor = DOM.Tag(Symbol("pluto-editor"))[ :class => "loading" ](
-        progress[
+        DOM.Tag(:progress)[
             :style => "filter:grayscale()",
             :class => "delete-me-when-live statefile-fetch-progress",
             :max => "100",
@@ -30,8 +25,8 @@ function Documenter.HTMLWriter.domify(dctx::DCtx, ::Node, block::PlutoBlock)
     )
 
     return [
-        script[Symbol("data-pluto-file") => "launch-parameters"](launch_params),
-        div[:style => "display:flex; overflow-y: hidden;"](editor),
+        DOM.Tag(:script)[Symbol("data-pluto-file") => "launch-parameters"](launch_params),
+        DOM.Tag(:div)[:style => "display:flex; overflow-y: hidden;"](editor),
     ]
 end
 
@@ -52,3 +47,14 @@ _is_absolute_url(s::AbstractString) =
 # our purposes (URLs), and Julia's repr() for String happens to produce a valid
 # JS string too. Use repr.
 _js_string(s::AbstractString) = repr(s)
+_js_string(s::Nothing) = "undefined"
+_js_string(s::Boolean) = repr(s)
+_js_string(s::Number) = repr(s)
+_js_string(s::JSLiteral) = s.x
+
+struct JSLiteral
+    x
+end
+
+
+
